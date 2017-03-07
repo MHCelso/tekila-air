805479b7dd7bf93de4d7f981a6d8c6d9f47b6c8c
Celso <furuyamic@gmail.com>
2017-03-07 13:47:31 -0600

    funcion y template base agregado

--- login/views.py
+++ login/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+# from django.shortcuts import render
+# from django.views import View
+from django.views.generic import TemplateView
 
-# Create your views here.
+# class IndexView(View):
+#	def get(self, request, *args, **kwargs):
+#		return render(request, 'base.
+
+class IndexView(TemplateView):
+    template_name = "base.html"
